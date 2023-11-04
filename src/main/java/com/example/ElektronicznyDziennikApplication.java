163d2b00f5b19924d2d738216286c32b0ba160e0
lukasz_komputer <lukasz6587@interia.pl>
2023-11-04 01:52:12 +0100

    Wersja startowa - ŁG

---
+++ src/main/java/com/example/ElektronicznyDziennikApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElektronicznyDziennikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElektronicznyDziennikApplication.class, args);
+	}
+
+}
